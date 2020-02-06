993a071ca8d15ae2325a71e5db8b8f04ad103bba
MinexAutomation <minex.automation@gmail.com>
2020-02-06 15:33:20 -0800

    Added VisualStudioSolutionFileProjectTypeGuidProvider implementation of IVisualStudioSolutionFileProjectTypeGuidProvider service.

--- source/R5T.Odense.Default/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Odense.Default
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Odense.Default/Code/Services/Implementations/VisualStudioSolutionFileProjectTypeGuidProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace R5T.Odense.Default
+{
+    public class VisualStudioSolutionFileProjectTypeGuidProvider : IVisualStudioSolutionFileProjectTypeGuidProvider
+    {
+        public Guid GetNetStandardLibraryProjectTypeGuid()
+        {
+            // This is the value created by the Visual Studio 2017 new .NET Standard library project.
+            var cSharpNetStandardLibrary = Guid.Parse("FAE04EC0-301F-11D3-BF4B-00C04F79EFBC");
+            return cSharpNetStandardLibrary;
+        }
+    }
+}
