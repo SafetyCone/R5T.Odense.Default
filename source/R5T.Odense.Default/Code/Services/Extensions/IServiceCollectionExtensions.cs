f00beb60d54cb3f7776600bfaac448028fc73b99
MinexAutomation <minex.automation@gmail.com>
2020-02-06 15:37:01 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Odense.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Odense.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileProjectTypeGuidProvider"/> implementation of <see cref="IVisualStudioSolutionFileProjectTypeGuidProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultVisualStudioSolutionFileProjectTypeGuidProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<IVisualStudioSolutionFileProjectTypeGuidProvider, VisualStudioSolutionFileProjectTypeGuidProvider>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileProjectTypeGuidProvider"/> implementation of <see cref="IVisualStudioSolutionFileProjectTypeGuidProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IVisualStudioSolutionFileProjectTypeGuidProvider> AddDefaultVisualStudioSolutionFileProjectTypeGuidProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioSolutionFileProjectTypeGuidProvider>(() => services.AddDefaultVisualStudioSolutionFileProjectTypeGuidProvider());
+            return serviceAction;
+        }
+    }
+}
