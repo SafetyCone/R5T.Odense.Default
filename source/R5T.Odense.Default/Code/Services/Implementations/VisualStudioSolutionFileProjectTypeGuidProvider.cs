9f5bc21b80ae9ff9af89fc6293f27840eaada968
davidcoats <david@getsafetycone.com>
2022-04-27 15:33:26 -0700

    Interim changes.

--- source/R5T.Odense.Default/Code/Services/Implementations/VisualStudioSolutionFileProjectTypeGuidProvider.cs
+++ source/R5T.Odense.Default/Code/Services/Implementations/VisualStudioSolutionFileProjectTypeGuidProvider.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Odense.Default
-{
-    public class VisualStudioSolutionFileProjectTypeGuidProvider : IVisualStudioSolutionFileProjectTypeGuidProvider
+{[ServiceImplementationMarker]
+    public class VisualStudioSolutionFileProjectTypeGuidProvider : IVisualStudioSolutionFileProjectTypeGuidProvider,IServiceImplementation
     {
         public Guid GetCSharpNetStandardLibraryProjectTypeGuid()
         {
